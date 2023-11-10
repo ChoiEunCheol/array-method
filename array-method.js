fe6cb4af2942c3cb5b2eddb3c60483a46ad70970
최은철 <wndqhrgkdlel@gmail.com>
2023-11-10 11:33:48 +0900

    3번 문제 완료

--- array-method.js
+++ array-method.js
@@ -28,3 +28,6 @@ console.log("1.",studentString);
 let outStudentName = studentList.pop();
 console.log("2.",outStudentName);
 
+// 3. 명단 중 세 번째 학생이 전학을 갔고, 새 학생이 그 자리에 왔습니다. 기존 학생을 제거하고 새 학생을 추가하는 코드를 작성하세요.
+studentList.splice(2,1,"새학생");
+console.log("3.",studentList);
